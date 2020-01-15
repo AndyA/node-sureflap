34488108a444f0088306ec37cc2f8b1658d0192e
Andy Armstrong <andy@hexten.net>
2020-01-15 15:53:09 +0000

    Refactor so everything's a SureFlap

--- index.js
+++ index.js
@@ -13,71 +13,7 @@ const PRODUCT_NAME = {
   6: "DualScan Cat Flap Connect"
 };
 
-class SureFlapObject {
-  constructor(sureflap, data) {
-    Object.assign(this, { sureflap, data });
-  }
-
-  get id() {
-    return this.data.id;
-  }
-
-  async _apiCall(method, path, payload) {
-    return this.sureflap._apiCall(method, path, payload);
-  }
-
-  async _apiFactory(clz, method, path, payload) {
-    return this.sureflap._apiFactory(clz, method, path, payload);
-  }
-}
-
-class SureFlapDevice extends SureFlapObject {
-  get productName() {
-    return PRODUCT_NAME[this.data.product_id] || "Unknown";
-  }
-
-  async control() {
-    return this._apiCall("GET", `/api/device/${this.id}/control`);
-  }
-
-  async setControl(opt) {
-    return this._apiCall("PUT", `/api/device/${this.id}/control`, opt);
-  }
-}
-
-class SureFlapPet extends SureFlapObject {
-  async position() {
-    return this._apiCall("GET", `/api/pet/${this.id}/position`);
-  }
-
-  async setPosition(where, since) {
-    const m = since ? moment(since) : moment();
-    return this._apiCall("POST", `/api/pet/${this.id}/position`, {
-      where,
-      since: m.toISOString()
-    });
-  }
-}
-
-class SureFlapHousehold extends SureFlapObject {
-  async devices() {
-    return this._apiFactory(
-      SureFlapDevice,
-      "GET",
-      `/api/household/${this.id}/device?with[]=control`
-    );
-  }
-
-  async pets() {
-    return this._apiFactory(
-      SureFlapPet,
-      "GET",
-      `/api/household/${this.id}/pet`
-    );
-  }
-}
-
-class SureFlap {
+class SureFlapAPI {
   constructor(opt) {
     this.opt = Object.assign(
       {
@@ -90,36 +26,55 @@ class SureFlap {
     );
   }
 
-  _api(path) {
+  static bless(optOrAPI) {
+    if (optOrAPI instanceof SureFlapAPI) return optOrAPI;
+    return new this(optOrAPI);
+  }
+
+  endpoint(path) {
     return this.opt.endpoint + path;
   }
 
-  async _apiCall(method, path, payload) {
-    const login = await this.login();
-    const opt = {
-      method,
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${login.token}`
-      }
+  async call(method, path, payload) {
+    const tryCall = async x => {
+      const login = await this.login();
+      const opt = {
+        method,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${login.token + (x || "")}`
+        }
+      };
+      if (payload) opt.body = JSON.stringify(payload);
+      const res = await fetch(this.endpoint(path), opt);
+      const msg = await res.json();
+      //      console.log(JSON.stringify({ path, opt, payload, msg }, null, 2));
+      return msg;
     };
-    if (payload) opt.body = JSON.stringify(payload);
-    if (payload) console.log(JSON.stringify({ path, payload }, null, 2));
-    const res = await fetch(this._api(path), opt);
-    const msg = await res.json();
-    if (msg.error)
-      throw new Error(`Failed ${path}: ${JSON.stringify(msg.error)}`);
-    return msg.data;
+
+    const t1 = await tryCall();
+    if (!t1.error) return t1.data;
+
+    if (!/not be verified/.test(t1.error.message || ""))
+      throw new Error(`Failed ${path}: ${JSON.stringify(t1.error)}`);
+
+    // retry
+    delete this._data;
+    const t2 = await tryCall();
+    if (t2.error)
+      throw new Error(`Failed ${path}: ${JSON.stringify(t2.error)}`);
+
+    return t2.data;
   }
 
-  async _apiFactory(clz, method, path, payload) {
-    const msg = await this._apiCall(method, path, payload);
-    if (_.isArray(msg)) return msg.map(obj => new clz(this, obj));
-    return new clz(this, msg);
+  async factory(clazz, path, payload) {
+    const msg = await this.call("GET", path, payload);
+    if (_.isArray(msg)) return msg.map(obj => new clazz(this, obj));
+    return new clazz(this, msg);
   }
 
   async _login() {
-    const res = await fetch(this._api("/api/auth/login"), {
+    const res = await fetch(this.endpoint("/api/auth/login"), {
       method: "POST",
       body: JSON.stringify(this.opt),
       headers: { "Content-Type": "application/json" }
@@ -137,16 +92,95 @@ class SureFlap {
   async logout() {
     if (!this._data) return;
 
-    await this._apiCall("POST", "/api/auth/logout");
+    await this.call("POST", "/api/auth/logout");
     delete this._data;
+  }
+}
+
+class SureFlap {
+  constructor(optOrAPI, data) {
+    this.api = SureFlapAPI.bless(optOrAPI);
+    this.data = data;
+  }
+
+  get id() {
+    return this.data && this.data.id;
+  }
+
+  async login() {
+    return this.api.login();
+  }
+
+  async logout() {
+    return this.api.logout();
   }
 
   async household(id) {
-    return this._apiCall("GET", `/api/household/${id}`);
+    return this.api.factory(SureFlapHousehold, `/api/household/${id}`);
   }
 
   async households() {
-    return this._apiFactory(SureFlapHousehold, "GET", "/api/household");
+    return this.api.factory(SureFlapHousehold, "/api/household");
+  }
+
+  async device(id) {
+    return this.api.factory(SureFlapDevice, `/api/device/${id}?with[]=control`);
+  }
+
+  async devices() {
+    return this.api.factory(SureFlapDevice, `/api/device?with[]=control`);
+  }
+
+  async pet(id) {
+    return this.api.factory(SureFlapPet, `/api/pet/${id}?with[]=position`);
+  }
+
+  async pets() {
+    return this.api.factory(SureFlapPet, "/api/pet?with[]=position");
+  }
+}
+
+class SureFlapDevice extends SureFlap {
+  get productName() {
+    return PRODUCT_NAME[this.data.product_id] || "Unknown";
+  }
+
+  async control() {
+    return this.api.call("GET", `/api/device/${this.id}/control`);
+  }
+
+  async setControl(opt) {
+    return this.api.call("PUT", `/api/device/${this.id}/control`, opt);
+  }
+}
+
+class SureFlapPet extends SureFlap {
+  async position() {
+    return this.api.call("GET", `/api/pet/${this.id}/position`);
+  }
+
+  async setPosition(where, since) {
+    const m = since ? moment(since) : moment();
+    return this.api.call("POST", `/api/pet/${this.id}/position`, {
+      where,
+      since: m.toISOString()
+    });
+  }
+}
+
+class SureFlapHousehold extends SureFlap {
+  async devices() {
+    return this.api.factory(
+      SureFlapDevice,
+      `/api/household/${this.id}/device?with[]=control`
+    );
+  }
+
+  async pets() {
+    return this.api.factory(
+      SureFlapPet,
+      `/api/household/${this.id}/pet?with[]=position`
+    );
   }
 }
 
